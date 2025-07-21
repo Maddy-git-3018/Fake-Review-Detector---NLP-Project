4d7cc11e081f1071f8cc765151bd958f4f6b1cde
Madhav Kanjilimadom <122287750+Maddy-git-3018@users.noreply.github.com>
2025-07-21 20:41:10 +0530

    added selenium

--- scraper/reviews.py
+++ scraper/reviews.py
@@ -6,15 +6,14 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_reviews(url):
-    # Set up headless Chrome
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())  # ✅ Setup Service properly
-    driver = webdriver.Chrome(service=service, options=chrome_options)  # ✅ Use named arguments
+    service = Service(ChromeDriverManager().install()) 
+    driver = webdriver.Chrome(service=service, options=chrome_options) 
 
     driver.get(url)
 
