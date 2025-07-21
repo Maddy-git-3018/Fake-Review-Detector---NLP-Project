83419dea662c75eafbef0a27860c447900a1fa76
Madhav Kanjilimadom <122287750+Maddy-git-3018@users.noreply.github.com>
2025-07-21 20:33:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,3 +12,5 @@ def convertmyTxt(rv):
     np = [c for c in rv if c not in string.punctuation]
     np = ''.join(np)
     return [w for w in np.split() if w.lower() not in stopwords.words('english')]
+
+#added stopwords func
